48301460f88551cc70f171487546f45454a5a9f7
Henrique Silva <henriquesilva@MBP-de-Henrique.lan>
2020-04-12 05:22:24 +0100

    Rework: Sequilize dynamic load modules

--- models/index.js
+++ models/index.js
@@ -1,15 +1,43 @@
 import Sequelize from "sequelize";
 import setupDb from "~/setup/database";
-import User from "~/models/user";
+import * as fs from "fs";
+import * as path from "path";
 
 const sequelize = setupDb();
-const models = {
-  User: User.init(sequelize, Sequelize),
-};
+
+let models = {};
+
+// Well, basically i did this script on my own to work with new Sequilize syntax
+// So don't trust
+async function loadModules(currPath) {
+  let stat = fs.lstatSync(currPath);
+  if (stat.isDirectory()) {
+    const files = fs.readdirSync(currPath);
+    let f,
+      l = files.length;
+    for (var i = 0; i < l; i++) {
+      f = path.join(currPath, files[i]);
+      if (files[i] != "index.js") {
+        const name = files[i].replace(".js", "");
+        if (require(path.join(currPath, files[i])).default)
+          models[
+            name.charAt(0).toUpperCase() + name.slice(1)
+          ] = require(path.join(currPath, files[i])).default;
+      }
+    }
+  }
+}
+
+loadModules(__dirname);
 
 Object.values(models)
-  .filter((model) => typeof model.associate === "function")
-  .forEach((model) => model.associate(models));
+  .map((model) => {
+    model.init(sequelize, Sequelize);
+    return model;
+  })
+  .forEach((model) => {
+    if (model.associate) model.associate();
+  });
 
 const db = {
   ...models,
