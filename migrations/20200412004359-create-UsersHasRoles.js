97554fc67fc042b4a30608868df74307ea3b8c8a
Henrique Silva <henriquesilva@MBP-de-Henrique.lan>
2020-04-12 05:21:09 +0100

    Add: Role, Idp, Many to many relationships migration scripts

---
+++ migrations/20200412004359-create-UsersHasRoles.js
@@ -0,0 +1,40 @@
+"use strict";
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("UsersHasRoles", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Users",
+          key: "id",
+        },
+      },
+      roleId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        references: {
+          model: "Roles",
+          key: "id",
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("UsersHasRoles");
+  },
+};
---
+++ migrations/20200412004406-create-UsersHasIdps.js
@@ -0,0 +1,44 @@
+"use strict";
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("UsersHasIdps", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Users",
+          key: "id",
+        },
+      },
+      IdpId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        references: {
+          model: "Idps",
+          key: "id",
+        },
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("UsersHasIdps");
+  },
+};
